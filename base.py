8f342f0992a258ffcc4806d7602938b9d112508c
Feyaz Baker <feyazabubaker@gmail.com>
2020-05-28 09:00:38 +0530

    Minor fix

--- base.py
+++ base.py
@@ -20,30 +20,30 @@ def main():
 
     frame = tk.Frame(mainwindow)
     frame.pack(padx=10, pady=10)
-    bankers_btn = tk.Button(frame, text="Banker's Algorithm", command=getinputBankers, fill='X')
-    bankers_btn.pack()
+    bankers_btn = tk.Button(frame, text="Banker's Algorithm", command=getinputBankers)
+    bankers_btn.pack(fill='x')
     CreateToolTip(bankers_btn, text = 'Generates a single safe sequence\n'
                  'Requires matrix input. Press tab to go to next input field faster!')
 
     frame = tk.Frame(mainwindow)
     frame.pack(padx=10, pady=10)
-    pagerep_btn = tk.Button(frame, text="Process Scheduling methods", command=scheduling, fill='X')
-    pagerep_btn.pack()
+    pagerep_btn = tk.Button(frame, text="Process Scheduling methods", command=scheduling)
+    pagerep_btn.pack(fill='x')
     CreateToolTip(pagerep_btn, text = 'Generates analysis of process scheduling methods\n'
                  'Gives turnaround time, waiting time, response time, and other statistics\n'
                  'for FCFS, SJF, SRTF, RR, preemptive and non preemptive Priority scheduling.')
 
     frame = tk.Frame(mainwindow)
     frame.pack(padx=10, pady=10)
-    pagerep_btn = tk.Button(frame, text="Page replacement methods", command=getinputPagerep, fill='X')
-    pagerep_btn.pack()
+    pagerep_btn = tk.Button(frame, text="Page replacement methods", command=getinputPagerep)
+    pagerep_btn.pack(fill='x')
     CreateToolTip(pagerep_btn, text = 'Generates analysis of page replacement methods\n'
                  'Gives number of page faults and frame contents for FIFO, LRU and OPTIMAL.')
 
     frame = tk.Frame(mainwindow)
     frame.pack(padx=10, pady=10)
-    pagerep_btn = tk.Button(frame, text="Memory Allocation methods", command=getinputMA, fill='X')
-    pagerep_btn.pack()
+    pagerep_btn = tk.Button(frame, text="Memory Allocation methods", command=getinputMA)
+    pagerep_btn.pack(fill='x')
     CreateToolTip(pagerep_btn, text = 'Generates analysis of Memory Allocation methods\n'
                  'Gives fragmentation, block allocation for first fit, best fit and worst fit.')
 
@@ -52,23 +52,23 @@ def main():
     frame = tk.Frame(mainwindow)
     frame.pack(padx=10, pady=10)
     disksched_btn = tk.Button(frame, text="Disk Scheduling methods", command=diskSchedulingMain)
-    disksched_btn.pack()
+    disksched_btn.pack(fill='x')
     CreateToolTip(disksched_btn, text = 'Implementation\n'
                  'Output is in textboxes')
 
     frame = tk.Frame(mainwindow)
     frame.pack(padx=10, pady=10)
     dinnerp = tk.Button(frame, text="Dining Philosopher", command = dp)
-    dinnerp.pack()
+    dinnerp.pack(fill='x')
     CreateToolTip(dinnerp, text = 'This is a simulation\n'
                  'Delay of 3 seconds to generate output\n'
                  'Simulates 5 philosophers in the dining philosophers problem')
     
     frame = tk.Frame(mainwindow)
     frame.pack(padx=10, pady=10)
-    dinnerp = tk.Button(frame, text="Sleeping Barbers", command = sb)
-    dinnerp.pack()
-    CreateToolTip(dinnerp, text = 'This is a simulation\n'
+    sleepyb = tk.Button(frame, text="Sleeping Barbers", command = sb)
+    sleepyb.pack(fill='x')
+    CreateToolTip(sleepyb, text = 'This is a simulation\n'
                  'Delay of a few seconds to generate output\n'
                  'Simulates 50 customers and 3 barbers in the sleeping barbers problem')
 
