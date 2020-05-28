1b362502e27005832d86a4e8949a1c7b7ab64996
Feyaz Baker <feyazabubaker@gmail.com>
2020-05-28 11:58:48 +0530

    descriptions pages

--- base.py
+++ base.py
@@ -2,13 +2,13 @@ import tkinter as tk
 from tkinter import ttk
 import numpy as np
 
-
 from bankers import *
 from page_rep import *
 from diskscheduling import *
 from processsynch import *
 from memoryalloc import *
 from procscheduling import *
+from descriptions import *
 
 def main():
     mainwindow = tk.Tk()
@@ -16,55 +16,58 @@ def main():
     mainwindow.minsize(600, 700)
 
     frame = tk.Frame(mainwindow)
-    frame.pack(padx=10, pady=10,fill='x')
+    frame.pack(padx=10, pady=10)
     bankers_btn = tk.Button(frame, text="Banker's Algorithm", command=getinputBankers)
-    bankers_btn.pack()
+    bankers_btn.pack(side=tk.LEFT)
     CreateToolTip(bankers_btn, text = 'Generates a single safe sequence\n'
                  'Requires matrix input. Press tab to go to next input field faster!')
+    desc = tk.Button(frame, text="Summary", command=bankers_desc)
+    desc.pack(side=tk.LEFT)
+
 
     frame = tk.Frame(mainwindow)
-    frame.pack(padx=10, pady=10,fill='x')
+    frame.pack(padx=10, pady=10)
     pagerep_btn = tk.Button(frame, text="Process Scheduling methods", command=scheduling)
-    pagerep_btn.pack()
+    pagerep_btn.pack(side=tk.LEFT)
     CreateToolTip(pagerep_btn, text = 'Generates analysis of process scheduling methods\n'
                  'Gives turnaround time, waiting time, response time, and other statistics\n'
                  'for FCFS, SJF, SRTF, RR, preemptive and non preemptive Priority scheduling.')
 
     frame = tk.Frame(mainwindow)
-    frame.pack(padx=10, pady=10,fill='x')
+    frame.pack(padx=10, pady=10)
     pagerep_btn = tk.Button(frame, text="Page replacement methods", command=getinputPagerep)
-    pagerep_btn.pack()
+    pagerep_btn.pack(side=tk.LEFT)
     CreateToolTip(pagerep_btn, text = 'Generates analysis of page replacement methods\n'
                  'Gives number of page faults and frame contents for FIFO, LRU and OPTIMAL.')
 
     frame = tk.Frame(mainwindow)
-    frame.pack(padx=10, pady=10,fill='x')
+    frame.pack(padx=10, pady=10)
     pagerep_btn = tk.Button(frame, text="Memory Allocation methods", command=getinputMA)
-    pagerep_btn.pack()
+    pagerep_btn.pack(side=tk.LEFT)
     CreateToolTip(pagerep_btn, text = 'Generates analysis of Memory Allocation methods\n'
                  'Gives fragmentation, block allocation for first fit, best fit and worst fit.')
 
 
 
     frame = tk.Frame(mainwindow)
-    frame.pack(padx=10, pady=10,fill='x')
+    frame.pack(padx=10, pady=10)
     disksched_btn = tk.Button(frame, text="Disk Scheduling methods", command=diskSchedulingMain)
-    disksched_btn.pack()
+    disksched_btn.pack(side=tk.LEFT)
     CreateToolTip(disksched_btn, text = 'Disk Schedluing Implementation\n'
                  'Output is in textboxes')
 
     frame = tk.Frame(mainwindow)
-    frame.pack(padx=10, pady=10,fill='x')
+    frame.pack(padx=10, pady=10)
     dinnerp = tk.Button(frame, text="Dining Philosopher", command = dp)
-    dinnerp.pack()
+    dinnerp.pack(side=tk.LEFT)
     CreateToolTip(dinnerp, text = 'This is a simulation\n'
                  'Delay of 3 seconds to generate output\n'
                  'Simulates 5 philosophers in the dining philosophers problem')
     
     frame = tk.Frame(mainwindow)
-    frame.pack(padx=10, pady=10,fill='x')
+    frame.pack(padx=10, pady=10)
     sleepyb = tk.Button(frame, text="Sleeping Barbers", command = sb)
-    sleepyb.pack()
+    sleepyb.pack(side=tk.LEFT)
     CreateToolTip(sleepyb, text = 'This is a simulation\n'
                  'Delay of a few seconds to generate output\n'
                  'Simulates 50 customers and 3 barbers in the sleeping barbers problem')
---
+++ descriptions.py
@@ -0,0 +1,41 @@
+import tkinter as tk
+
+def bankers_desc():
+    root = tk.Tk()
+    root.title("Banker's Algorithm")
+    root.minsize(600, 700)
+
+    desc_frame = tk.Frame(root, background='gray')
+    desc_frame.pack(padx=10, pady=10, fill='both', expand=tk.YES)
+
+    def on_configure(event):
+        canvas.configure(scrollregion=canvas.bbox('all'))
+
+    canvas = tk.Canvas(desc_frame, background='gray')
+    canvas.pack(side=tk.LEFT, fill='both', expand=tk.YES)
+
+    scrollbar = tk.Scrollbar(desc_frame, command=canvas.yview)
+    scrollbar.pack(side=tk.LEFT, fill='y')
+
+    canvas.configure(yscrollcommand = scrollbar.set)
+
+    canvas.bind('<Configure>', on_configure)
+
+
+    frame = tk.Frame(canvas, background='gray')
+    canvas.create_window((0,0), window=frame, anchor='nw')
+
+
+    lbl = tk.Label(frame, text="Banker's Algorithm", font="-size 20", background='gray')
+    lbl.pack()
+
+    tbox = tk.Text(frame, background='gray', wrap=tk.WORD)
+    tbox.pack()
+    tbox.insert(tk.INSERT, "This project is submitted to Shashidhar sir, Anappa Sir, and \nAnkit Sir, of the Computer Science department of NITK, Surathkal, \nas part of the course requirements for CS255, Operating Systems\n\n")
+
+
+    tbox.config(state='disabled')
+
+
+
+    root.mainloop()
